9e461f1fb09ccfc46fa37c503f5a4f78624ce2ac
z01a <5a6f4c41@gmail.com>
2022-09-02 00:02:49 +0200

    Added Default and Login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './components/default/default.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: DefaultComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
