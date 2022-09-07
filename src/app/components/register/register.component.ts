11c395abf99e85351050195f16496896b0dce532
z01a <5a6f4c41@gmail.com>
2022-09-07 11:27:39 +0200

    Added UI for RegisterComponent

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.errorMessage = "";
+    this.requestInProgress = false;
+  }
 
   ngOnInit(): void {
+  }
+
+  registerGroup = new FormGroup({
+    firstname: new FormControl('', [Validators.required]),
+    lastname: new FormControl('', [Validators.required]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    repeated: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    address: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+  });
+
+  hideRequiredControl = new FormControl(true);
+
+  requestInProgress : boolean;
+
+  errorMessage : string;
+
+  performRegister() {
+
   }
 
 }
