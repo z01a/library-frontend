628fc1fca62fdc5039e69fdf00dc9cff0bd80566
z01a <5a6f4c41@gmail.com>
2022-09-14 00:07:19 +0200

    Added overview of books for moderator and ability to delete them

--- src/app/components/application/moderator/books/books.component.ts
+++ src/app/components/application/moderator/books/books.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Book } from 'src/app/models/book';
+import { BooksService } from 'src/app/services/books.service';
 
 @Component({
   selector: 'app-books',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModeratorBooksComponent implements OnInit {
 
-  constructor() { }
+  constructor(private booksService: BooksService, private router: Router) { }
 
   ngOnInit(): void {
+    this.fetchBooks()
+  }
+
+  displayedColumns: string[] = ['title', 'author', 'publisher', 'delete'];
+
+  books: Book[] = []
+
+  fetchBooks() {
+    this.booksService.fetch(false).subscribe({
+      next: (result: any) => {
+        this.books = result
+        console.log(this.books)
+      }
+    });
+  }
+
+  performRowAction(isbn: string) {
+    this.router.navigate(['/moderator/books/' + isbn]);
+  }
+
+  performDelete(isbn: string) {
+    this.booksService.delete(isbn).subscribe({
+      next: (result) => {
+        if(result) {
+          this.books = []
+          this.fetchBooks()
+        }
+      }
+    })
   }
 
 }
