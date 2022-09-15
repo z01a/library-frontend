44a56f8b3b5685e1eaeda59466c846e6bd6ec55b
z01a <5a6f4c41@gmail.com>
2022-09-15 19:33:54 +0200

    Added config and also fetching config when needed

--- src/app/components/application/user/reading/reading.component.ts
+++ src/app/components/application/user/reading/reading.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { take } from 'rxjs';
+import { AppComponent } from 'src/app/app.component';
 import { Book } from 'src/app/models/book';
 import { BooksService } from 'src/app/services/books.service';
+import { ConfigService } from 'src/app/services/config.service';
 
 @Component({
   selector: 'app-reading',
@@ -11,13 +13,24 @@ import { BooksService } from 'src/app/services/books.service';
 })
 export class UserReadingComponent implements OnInit {
 
-  constructor(private booksService: BooksService, private snackbar: MatSnackBar) { }
+  constructor(private booksService: BooksService, private configService: ConfigService, private snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.fetchConfig()
     this.fetchBooks()
   }
 
   books: any = []
+
+  maxLoanDays: number = 14;
+
+  fetchConfig() {
+    this.configService.fetch().subscribe({
+      next: (config: any) => {
+        this.maxLoanDays = config.maxLoanDays
+      }
+    });
+  }
 
   fetchBooks() {
     this.booksService.fetchMyBooks().subscribe({
@@ -48,7 +61,7 @@ export class UserReadingComponent implements OnInit {
 
     let differenceInTime = todayDate.getTime() - sentOnDate.getTime()
 
-    return Math.floor(differenceInTime / (1000 * 3600 * 24));
+    return this.maxLoanDays - Math.floor(differenceInTime / (1000 * 3600 * 24));
   }
 
 }
